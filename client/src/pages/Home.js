6950047ae1b7edf483fd68e451d2360ca9e763dc
JonelBriones <jonel.c.briones@gmail.com>
2022-08-26 00:02:44 -0700

    adding client folder

---
+++ client/src/pages/Home.js
@@ -0,0 +1,88 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <div id="navbar">
+        <div className="navlinks">
+          <h1>Strength.</h1>
+          <ul>
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Programs</a>
+            </li>
+            <li>
+              <a href="">Cart</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div id="showcase">
+        <div>
+          <h1>Strength is a reward.</h1>
+          <button className="btn white">Choose your Program</button>
+        </div>
+      </div>
+      <main className="container">
+        <header className="programs__header">
+          <h1>Explore our programs that fit your style</h1>
+        </header>
+        <section className="programs">
+          <div className="programs__content grid">
+            <div className="programs__card">
+              <img
+                src="/images/sushil-ghimire-eFx1KZhaSvo-unsplash.jpg"
+                alt=""
+              />
+              <div className="programs__name">
+                <h1>Program 1</h1>
+                <p>Best for those trying to lose weight</p>
+                <button className="btn white">Add to Cart</button>
+              </div>
+            </div>
+            <div className="programs__card">
+              <img
+                src="/images/alonso-reyes-0HlI76m4jxU-unsplash.jpg"
+                alt="picture"
+              />
+              <div className="programs__name">
+                <h1>Program 1</h1>
+                <p>Best for those trying to lose weight</p>
+                <button className="btn white">Add to Cart</button>
+              </div>
+            </div>
+            <div className="programs__card">
+              <img
+                src="/images/hayley-kim-design-eot-ka5dM7Q-unsplash.jpg"
+                alt=""
+              />
+              <div className="programs__name">
+                <h1>Program 1</h1>
+                <p>Best for those trying to lose weight</p>
+                <button className="btn white">Add to Cart</button>
+              </div>
+            </div>
+            <div className="programs__card">
+              <img
+                src="/images/john-fornander-TAZoUmDqzXk-unsplash.jpg"
+                alt=""
+              />
+              <div className="programs__name">
+                <h1>Program 1</h1>
+                <p>Best for those trying to lose weight</p>
+                <button className="btn white">Add to Cart</button>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default Home;
